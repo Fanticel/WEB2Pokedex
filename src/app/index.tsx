16468822699b12b5f29ea72b9080fa2929aeb93f
Fanticel <duzywiewior@tlen.pl>
2025-04-27 20:58:37 +0200

    Man im trying

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,11 @@ import { AppProvider } from './provider';
 import { AppRouter } from './router';
 
 export const App = () => {
+  if (process.env.NODE_ENV === 'development' || window.location.hostname === 'fanticel.github.io') {
+    const { startWorker } = require('./mocks/browser');
+    startWorker().catch(console.error);
+  }
+  
   return (
     <AppProvider>
       <AppRouter />
